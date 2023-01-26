b372c68c53bb5eaaf47f7e0d9af8a481968dc702
dev_traveler <avenj.dev@gmail.com>
2023-01-26 16:25:52 +0900

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by JAEHYUN KIM on 2023/01/26.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
